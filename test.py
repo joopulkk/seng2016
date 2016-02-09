7db8367347ba9aa2325a6c7f78f4f633f3db06ee
joopulkk <joonas.l.pulkkinen@student.oulu.fi>
2016-02-09 10:40:51 +0200

    Korjattu testi versiolle 1

--- test.py
+++ test.py
@@ -18,14 +18,24 @@ class TestSuite(unittest.TestCase):
 
         self.failIf(len(output.getvalue().splitlines()) != 100)
 		
-    def test_ver1(self):
+    def test_Fizz(self):
         app =FizzBuzz()
         self.failIf(app.calc(3) != "Fizz")
+        self.failIf(app.calc(21) != "Fizz")
+        self.failIf(app.calc(63) != "Fizz")
+        
+        
+    def test_Buzz(self):
+        app =FizzBuzz()
         self.failIf(app.calc(5) != "Buzz")
+        self.failIf(app.calc(10) != "Buzz")
+        self.failIf(app.calc(25) != "Buzz")
+		
+    def test_FizzBuzz(self):
+        app =FizzBuzz()
         self.failIf(app.calc(15) != "FizzBuzz")
-	
-		
-		
+        self.failIf(app.calc(45) != "FizzBuzz")
+        self.failIf(app.calc(60) != "FizzBuzz")
 
 def main():
     unittest.main()
